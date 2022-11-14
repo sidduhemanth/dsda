c5a027dcf0710d3743d474edb9e68d6486586cd7
sidduhemanth <ssiddu8619@gmail.com>
2022-11-14 15:02:28 +0530

    change1

---
+++ matrix.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 14 14:53:03 2022
+
+@author: cse
+"""
+import numpy as np
+matrix_2*2=np.array([[1,2],
+                     [3,4]])
+matrix_3*3=np.array([[1,2,3],
+                     [4,5,6],
+                     [8,9,10]])
+print("2*2 matrix/n",matrix_2*2,"/n Shape of matrix->",matrix_2*2.Shape)
+print("3*3 matrix/n",matrix_3*3,"/n Shape of matrix->",matrix_3*3.Shape)
